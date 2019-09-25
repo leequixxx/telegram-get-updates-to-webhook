f6f51e715cc7702f51f0c8ed4e5b61f5604296cb
Misha Vorobeychikov <leequixxx@io.jupiter.sun>
2019-09-26 00:43:13 +0300

    feat: added config file

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const axios                 = require('axios');
+const nconf                 = require('nconf');
 const { setIntervalAsync }  = require('set-interval-async/fixed');
+
+const config = nconf.file({ file: 'config.json' });
 
 let lastUpdateId = 0;
 
 setIntervalAsync(async () => {
-  const { data } = await axios.get(`https://api.telegram.org/${process.env.BOT_TOKEN}/getUpdates?offset=${lastUpdateId}`);
+  const { data } = await axios.get(`https://api.telegram.org/${config.get('bot:token')}/getUpdates?offset=${lastUpdateId}`);
   data.result.forEach(async (update) => {
     lastUpdateId = update.update_id;
     lastUpdateId++;
     console.log(update.message.text);
-    await axios.post(process.env.BOT_WEBHOOK, update);
+    await axios.post(config.get('bot:webhook'), update);
   });
 }, 1000);
